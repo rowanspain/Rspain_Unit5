857db00716ed25afbec61137458dbd5dc3783b13
rowanspain <125788793+rowanspain@users.noreply.github.com>
2023-11-19 21:44:24 -0800

    LessonB

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private const float spawnRate = 2.0f;
+    public List<GameObject> prefabs;
+
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+        }
+    }
+
+    public void UpdateScore(int scoreDelta)
+    {
+        score += scoreDelta;
+        if(score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = "Score: " + score;
+    }
+}
