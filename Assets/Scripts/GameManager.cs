1a7c0eba0803192cc5e4e49a8e26e373795ad7fa
rowanspain <125788793+rowanspain@users.noreply.github.com>
2023-11-19 20:23:56 -0800

    LessonA

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const float spawnRate = 2.0f;
+    public List<GameObject> prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+        }
+    }
+}
